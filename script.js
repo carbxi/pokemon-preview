184a915fa82de46dd1921992ce81486efe00b149
carbxi <rayofgrey@gmail.com>
2026-07-23 18:18:14 -0300

    fixed naming and styling schemes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const inputForm = document.getElementById("input-form");
+const inputForm = document.getElementById("add-form");
 inputForm.addEventListener('submit', async (event) => {
   event.preventDefault()
-  const input = document.getElementById("input-text");
+  const input = document.getElementById("add-input");
   insertPokemon(input.value);
   input.value = "";
 })
@@ -27,61 +27,66 @@ const colourMap = {
   fairy: '#D685AD',
 };
 
-function createCard(object) {
-  const cardTemplate = document.getElementById("card-template").content.cloneNode(true);
-  const card = cardTemplate.querySelector('.card');
-  card.style.backgroundColor = object.colours[0];
-  card.querySelector('.header-name').textContent = object.name;
-  card.querySelector('.header-id').textContent = object.id;
-  card.querySelector('.card-image').src = object.standardImage;
-  preloadImage(object.shinyImage);
-  card.querySelector('.about').textContent = object.info;
-  if (object.colours.length > 1) {
-    card.style.background = `linear-gradient(to bottom, ${object.colours[0]}, ${object.colours[1]})`;
+function createCard(pokemon) {
+  const card = document.getElementById("card-template").content.querySelector('.card').cloneNode(true);
+
+  card.querySelector('.header-name').textContent = pokemon.displayName;
+  card.querySelector('.header-id').textContent = pokemon.id;
+  card.querySelector('.card-image').src = pokemon.standardImage;
+  card.querySelector('.about').textContent = pokemon.about;
+
+  //Image setup
+  preloadImage(pokemon.shinyImage);
+  card.dataset.shinyImage = pokemon.shinyImage;
+  card.dataset.standardImage = pokemon.standardImage;
+
+  //Card Backgrounds
+  card.style.backgroundColor = pokemon.backgroundColours[0];
+  if (pokemon.backgroundColours.length > 1) {
+    const primaryColour = pokemon.backgroundColours[0];
+    const secondaryColour = pokemon.backgroundColours[1];
+    card.style.background = `linear-gradient(to bottom, ${primaryColour}, ${secondaryColour})`;
   }
-  card.dataset.standardImage = object.standardImage;
-  card.dataset.shinyImage = object.shinyImage;
-  console.log(card.dataset.shinyImage);
-  return cardTemplate;
+
+  return card;
 }
 
 async function createPokemon(name) {
   const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-  const pokemon = await pokemonResponse.json();
+  const pokemonData = await pokemonResponse.json();
 
-  const speciesResponse = await fetch(pokemon.species.url);
-  const species = await speciesResponse.json();
+  const speciesResponse = await fetch(pokemonData.species.url);
+  const speciesData = await speciesResponse.json();
 
-  const entries = species.flavor_text_entries;
+  const dexEntries = speciesData.flavor_text_entries;
   let i = 0;
-  while (entries[i].language.name != "en") {
+  while (dexEntries[i].language.name != "en") {
     i++;
   }
 
+  const colours = pokemonData.types.map((type) => colourMap[type.type.name]);
 
-  const colours = pokemon.types.map((type) => colourMap[type.type.name]);
-
-  let object = {
-    name: formatName(pokemon.name),
-    id: `#${species.id}`,
-    info: species.flavor_text_entries[i].flavor_text.replace(/[\s\f]+/g, ' '),
-    standardImage: pokemon.sprites.other['official-artwork'].front_default,
-    shinyImage: pokemon.sprites.other['official-artwork'].front_shiny,
-    colours: colours,
+  let pokemon = {
+    displayName: formatName(pokemonData.name),
+    id: `#${speciesData.id}`,
+    about: speciesData.flavor_text_entries[i].flavor_text.replace(/[\s\f]+/g, ' '),
+    standardImage: pokemonData.sprites.other['official-artwork'].front_default,
+    shinyImage: pokemonData.sprites.other['official-artwork'].front_shiny,
+    backgroundColours: colours,
   };
 
-  return object;
+  return pokemon;
 }
 
 async function insertPokemon(name) {
   const pokemon = await createPokemon(name);
   const card = createCard(pokemon);
-  document.getElementById("container").append(card);
+  document.getElementById("main-container").append(card);
 }
 
 function clearPage() {
-  document.getElementById("input-text").value = "";
-  const container = document.getElementById("container");
+  document.getElementById("add-input").value = "";
+  const container = document.getElementById("main-container");
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
@@ -99,7 +104,7 @@ async function fillAll() {
 function toggleImage(card) {
   let image = card.querySelector('.card-image');
   if (card.dataset.shinyImage) {
-    image.src = image.src == card.dataset.standardImage ? card.dataset.shinyImage : card.dataset.standardImage;
+    image.src = image.src === card.dataset.standardImage ? card.dataset.shinyImage : card.dataset.standardImage;
   }
 }
 
